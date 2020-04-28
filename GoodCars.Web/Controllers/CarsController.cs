589fe3366c209acc34947e65297cd3c02c7d8d0b
Anatoliy Novikov <chamskra66er@gmail.com>
2020-04-28 14:50:08 +0300

    refac CarsController

---
+++ GoodCars.Web/Controllers/CarsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GoodCars.Web.Controllers
+{
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+
+        private readonly ILogger<CarsController> _logger;
+
+        public CarsController(ILogger<CarsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/api/cars")]
+        public ActionResult GetCars()
+        {
+            return Ok("Cars!");
+        }
+    }
+}
