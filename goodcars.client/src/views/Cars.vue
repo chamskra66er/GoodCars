a1f0bf8fb65fab79713cbe2d4fa4f42cc6147b02
Anatoliy Novikov <chamskra66er@gmail.com>
2020-05-03 08:01:16 +0300

    added action for Cars.vue

--- goodcars.client/src/views/Cars.vue
+++ goodcars.client/src/views/Cars.vue
@@ -3,3 +3,31 @@
     <h1>Car list view</h1>
   </div>
 </template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    import ICar from '@/types/Car';
+    import CarService from '../services/cars-service';
+
+    const carService = new CarService();
+
+    @Component({
+        name: 'MyCars',
+        components: {}
+    })
+    export default class MyCars extends Vue {
+        myCars: ICar[] = [];
+        get carCount() {
+            return this.myCars.length;
+        }
+        created() {
+            carService.getAllCars().
+                then(result => this.myCars = result);
+        }
+
+    }
+</script>
+
+<style lang="scss">
+
+</style>
